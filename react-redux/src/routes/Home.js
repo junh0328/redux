bde5dbc82f785d4c4dcc8abc1a5d377582a43f79
이준희 <junh0328@naver.com>
2020-11-05 15:33:02 +0900

    react-redux 초기세팅 3

--- react-redux/src/routes/Home.js
+++ react-redux/src/routes/Home.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>This is a homepage.</div>;
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+    setText("");
+  };
+  return (
+    <div>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </div>
+  );
 };
 
 export default Home;
